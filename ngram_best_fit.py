cdaaf56017b760a0873ba352c6f003d4b7b8fdbd
jgippetti <jgippett@stanford.edu>
2014-10-19 17:29:22 -0700

    modified file that returns language model

--- ngram_best_fit.py
+++ ngram_best_fit.py
@@ -8,12 +8,12 @@ def sliding(xs, windowSize):
     for i in xrange(1, len(xs) + 1):
         yield xs[max(0, i - windowSize):i]
 
-# def removeAll(s, chars):
-#     return ''.join(filter(lambda c: c not in chars, s))
+def removeAll(s, chars):
+      return ''.join(filter(lambda c: c not in chars, s))
 
-# def alphaOnly(s):
-#     s = s.replace('-', ' ')
-#     return filter(lambda c: c.isalpha() or c == ' ', s)
+def alphaOnly(s):
+     s = s.replace('-', ' ')
+     return filter(lambda c: c.isalpha() or c == ' ', s)
 
 def cleanLine(l):
     return alphaOnly(l.strip().lower())
@@ -41,13 +41,28 @@ def makeLanguageModels(path,n):
             ngrams = [ngramWindow(x) for x in sliding(ws, n)]
             ngramsCounts.update(ngrams)
             ntotalCounts.update([x[0] for x in ngrams])
-
     vocab_size  = len(ntotalCounts)
  
     def ngramModel(n_length_list):
         if len(n_length_list) != n:
                 raise Exception("Need to input list of elements of length n!")
-        return math.log(ntotalCounts[n_length_list[0]] + vocab_size) - math.log(ngramsCounts[(tuple(n_length_list))] + 1) 
+        word_count = 0
+        for i in range(n-1):
+            word_count += ntotalCounts[n_length_list[i]]
+        return math.log(word_count + vocab_size) - math.log(ngramsCounts[(tuple(n_length_list))] + 1) 
 
-    return ngramModel
+    def ngramFindNextWord(text_input):
+        if len(text_input) != n-1:
+            print(text_input)
+            raise Exception("Need to input list of elements of length n-1!")
+        min_cost = float("inf")
+        best_fit = ''
+        for word in set(ntotalCounts):
+            if ngramModel(text_input + [word]) < min_cost:
+                min_cost = ngramModel(text_input + [word])
+                best_fit = word
+        return best_fit
+
+    return ngramFindNextWord
+
 
