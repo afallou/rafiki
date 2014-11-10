a511d89a0e8b7fae6c59f873ee160b26d31f4d8e
Susan Tu <susanctu@gmail.com>
2014-11-09 21:52:33 -0800

    wip

--- factorGraphModels/HMMModel.py
+++ factorGraphModels/HMMModel.py
@@ -1,30 +1,34 @@
 import argparse
 import sys, os, subprocess
 from constructProbs import TransitionProbsBuilder
+import itertools
 
 """
 	Return a list of filenames 
 """
 def getDirpaths(root, keep_fn=lambda x: True):
-	return filter(keep_fn, [dirpath for dirpath, _, _ in os.walk(root)])
+	filepaths = [ [dirpath + '/' + fname for fname in filenames] for dirpath, _, filenames in os.walk(root)]
+	flattened = list(itertools.chain(*filepaths))
+	print(flattened)
+	return filter(keep_fn, flattened)
 
 def main():
 	parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'root', help='the directory to start recursively searching from')
-    
-    # can make the following option-controlled later if we start looking at other langs
-    def isPythonFile(dirpath):
-    	return dirpath[len(dirpath)-3:len(dirpath)] == ".py"
+	parser.add_argument(
+		'root', help='the directory to start recursively searching from')
+	
+	# can make the following option-controlled later if we start looking at other langs
+	def isPythonFile(dirpath):
+		return dirpath[len(dirpath)-3:len(dirpath)] == ".py"
 
-    args = parser.parse_args()
-    dirpaths = getDirpaths(os.path.expanduser(args.root), isPythonFile)
-    transProbBuilder = TransitionProbsBuilder()
-    for dirpath in dirpaths:
-    	transProbBuilder.updateTransitionProbs(dirpath)
+	args = parser.parse_args()
+	dirpaths = getDirpaths(os.path.expanduser(args.root), isPythonFile)
+	transProbBuilder = TransitionProbsBuilder()
+	for dirpath in dirpaths:
+		transProbBuilder.updateTransitionProbs(dirpath)
 
-   	transProb = transProbBuilder.build()
-   	print transProb
+	transProb = transProbBuilder.build()
+	
 
 if __name__ == "__main__":
 	main()
--- factorGraphModels/constructProbs.py
+++ factorGraphModels/constructProbs.py
@@ -5,63 +5,71 @@ import io
 
 
 
-class NormalizingDict:
+class ImmutableNormalizingDict:
   """
-  	Warning: incomplete dict interface! May fix later
+    Warning: incomplete dict interface! May fix later
   """
-  def __init__(self, python_dict={}, sum_fn=sum):
-  	self.sum_fn = sum_fn
+  def __init__(self, python_dict, sum_fn=sum):
     self.store = python_dict
-  
-  def __setitem__(self, key, value):
-  	# values should be floats if you didn't provide a special sum_fn
-    self.store[key] = value
+    self.summed = sum_fn(self.store.values())
 
   def __getitem__(self, key):
-  	return self.store[key] / self.sum_fn(self.store.values())
+    return self.store[key] / self.summed
 
   def __contains__(self, key):
-  	return key in self.store
+    return key in self.store
+
+  def __str__(self):
+    return str(self.store)
 
 """
-	Return:
-		transProb such that transProb[w0][sep][w1] gives you the count 
+    Return:
+        transProb such that transProb[w0][sep][w1] gives you the count 
 """
 class TransitionProbsBuilder:
- 	# we're going to treat every thing other than name, string, number as a separator
+    # we're going to treat every thing other than name, string, number as a separator
 
- 	def __init__():
- 		self.transProb = {}
+    def __init__(self):
+        self.transProb = {}
 
-	def updateTransitionProbs(dirpath):
-		with open(dirpath,'r') as f:
-    		content=f.read()
-			g = tokenize.generate_tokens(io.BytesIO(content).readline) 
-			prev_non_sep_tok = None
-			prev_sep = None # this is the separator that comes after prev_non_sep_tok
-			for toknum, tokval, _, _, _  in g:
-				# we see a token that is not a separator
-				if toknum != token.NAME and toknum != token.NUMBER and toknum != token.STRING:
-					if prev_non_sep_tok != None and prev_sep != None:
-						if prev_non_sep_tok in self.transProb:
-							if prev_sep in self.transProb[prev_non_sep_tok]:
-								if toknum in self.transProb[prev_non_sep_tok][prev_sep]:
-									self.transProb[prev_non_sep_tok][prev_sep][toknum] +=1
-								else:
-									self.transProb[prev_non_sep_tok][prev_sep][toknum] = 1
-							else:
-								self.transProb[prev_non_sep_tok][prev_sep] = NormalizingDict({toknum: 1})
+    def updateTransitionProbs(self, dirpath):
+        with open(dirpath,'r') as f:
+            content=f.read()
+            g = tokenize.generate_tokens(io.BytesIO(content).readline) 
+            prev_non_sep_tok = None
+            prev_sep = None # this is the separator that comes after prev_non_sep_tok
+            for toknum, tokval, _, _, _  in g:
+                # we see a token that is not a separator
+                if toknum == tokenize.COMMENT:
+                    continue
+                if toknum == token.NAME:
+                    if prev_non_sep_tok != None and prev_sep != None:
+                        if prev_non_sep_tok in self.transProb:
+                            if prev_sep in self.transProb[prev_non_sep_tok]:
+                                if tokval in self.transProb[prev_non_sep_tok][prev_sep]:
+                                    self.transProb[prev_non_sep_tok][prev_sep][tokval] += 1
+                                else:
+                                    self.transProb[prev_non_sep_tok][prev_sep][tokval] = 1
+                            else:
+                                self.transProb[prev_non_sep_tok][prev_sep] = {tokval: 1}
 
-						else:
-							self.transProb[prev_non_sep_tok] = {prev_sep: NormalizingDict({toknum: 1})}
-					prev_non_sep_tok = toknum 
-					prev_sep = None
-				else: # we see a token that is a separator 
-					if prev_non_sep_tok != None:
-						prev_sep = toknum # can we have two separators in a row? in that case we'll take the last, which may not be the right thing to do....
+                        else:
+                            self.transProb[prev_non_sep_tok] = {prev_sep: {tokval: 1}}
+                    prev_non_sep_tok = tokval
+                    prev_sep = None
+                elif toknum != token.NAME and toknum != token.NUMBER and toknum != token.STRING: # we see a token that is a separator 
+                    if prev_non_sep_tok != None:
+                        if prev_sep is None:
+                            prev_sep = tokval
+                        else:
+                            prev_sep = prev_sep + tokval # multiple separators in a row; treat as a special separator
 
-	def build():
-		return self.transProb 
+    def build(self):
+        # replace all the innermost dicts with ImmutableNormalizingDicts
+        for key1 in self.transProb:
+            for key2 in self.transProb[key1]:
+                self.transProb[key1][key2] = ImmutableNormalizingDict(self.transProb[key1][key2])
+        return self.transProb 
 
 
-	
+    
