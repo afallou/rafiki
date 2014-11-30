578e91dbc980c86b2e504965a04914f850941bae
Susan Tu <susanctu@gmail.com>
2014-11-29 17:21:39 -0800

    refactored updateTransitionProbs

--- factorGraphModels/constructProbs.py
+++ factorGraphModels/constructProbs.py
@@ -231,35 +231,32 @@ class TransitionProbsBuilder:
 
     def updateTransitionProbs(self, dirpath):
         with open(dirpath,'r') as f:
-            content=f.read()
-            g = tokenize.generate_tokens(io.BytesIO(content).readline)
-            prev_non_sep_tok = None
-            prev_sep = None # this is the separator that comes after prev_non_sep_tok
-            for sep, tokval  in getSeparatorAndToken(g):
-                # we see a token that is not a separator
-                if toknum == tokenize.COMMENT:
-                    continue
-                if toknum == token.NAME:
-                    if prev_non_sep_tok != None and prev_sep != None:
+            totalLineCount = sum(1 for line in f)
+            f.seek(0)
+            stopTrainLine = int(totalLineCount * (1 - float(self.percentTest)))
+            lineCount = 0
+            for line in f:
+                if lineCount >= stopTrainLine:
+                    break
+                g = tokenize.generate_tokens(io.BytesIO(line).readline)
+                prev_non_sep_tok = None
+                for prev_sep, maybe_name  in getSeparatorAndToken(g):
+                    assert((prev_non_sep_tok is None and sep is None) or (prev_non_sep_tok is not None and sep is not None))
+                    if prev_non_sep_tok is not None and sep is not None:
                         if prev_non_sep_tok in self.transProb:
                             if prev_sep in self.transProb[prev_non_sep_tok]:
-                                if tokval in self.transProb[prev_non_sep_tok][prev_sep]:
-                                    self.transProb[prev_non_sep_tok][prev_sep][tokval] += 1
+                                if maybe_name in self.transProb[prev_non_sep_tok][prev_sep]:
+                                    self.transProb[prev_non_sep_tok][prev_sep][maybe_name] += 1
                                 else:
-                                    self.transProb[prev_non_sep_tok][prev_sep][tokval] = 1
+                                    self.transProb[prev_non_sep_tok][prev_sep][maybe_name] = 1
                             else:
-                                self.transProb[prev_non_sep_tok][prev_sep] = {tokval: 1}
+                                self.transProb[prev_non_sep_tok][prev_sep] = {maybe_name: 1}
 
                         else:
-                            self.transProb[prev_non_sep_tok] = {prev_sep: {tokval: 1}}
-                    prev_non_sep_tok = tokval
-                    prev_sep = None
-                elif toknum != token.NAME and toknum != token.NUMBER and toknum != token.STRING: # we see a token that is a separator
-                    if prev_non_sep_tok != None:
-                        if prev_sep is None:
-                            prev_sep = tokval
-                        else:
-                            prev_sep = prev_sep + tokval # multiple separators in a row; treat as a special separator
+                            self.transProb[prev_non_sep_tok] = {prev_sep: {maybe_name: 1}}
+                    
+                    prev_non_sep_tok = maybe_name
+                lineCount += 1
 
     def build(self):
         # replace all the innermost dicts with ImmutableNormalizingDicts
