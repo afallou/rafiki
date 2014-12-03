2982016428923a33d6c1b6ce099586489ce285e2
Adrien Fallou <adrien.fallou@gmail.com>
2014-12-02 18:54:21 -0800

    Particle filtering main structure

---
+++ factorGraphModels/util.py
@@ -0,0 +1,21 @@
+# Function: Weighted Random Choice
+# --------------------------------
+# Given a dictionary of the form element -> weight, selects an element
+# uniformly over the different weights.
+def weightedRandomChoice(weightDict):
+    weights = []
+    elems = []
+    for elem in weightDict:
+        weights.append(weightDict[elem])
+        elems.append(elem)
+    total = sum(weights)
+    key = random.uniform(0, total)
+    runningTotal = 0.0
+    chosenIndex = None
+    for i in range(len(weights)):
+        weight = weights[i]
+        runningTotal += weight
+        if runningTotal > key:
+            chosenIndex = i
+            return elems[chosenIndex]
+    raise Exception('Should not reach here')
