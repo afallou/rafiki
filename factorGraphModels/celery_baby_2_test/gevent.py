90b653c50a9c596765fb4bac1a1874b1ebf7ffa4
jgippetti <jgippett@stanford.edu>
2014-12-03 11:57:08 -0800

    poster updates

--- factorGraphModels/celery_baby_2_test/gevent.py
+++ factorGraphModels/celery_baby_2_test/gevent.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from time import time
-try:
+except ImportError:
     from gevent import Timeout
 except ImportError:
     Timeout = None
@@ -9,14 +9,14 @@ from .base import apply_target, BasePool
 def apply_timeout(target, apply_target=apply_target, **rest):
 from __future__ import absolute_import
 from time import time
-try:
+except ImportError:
     from gevent import Timeout
 except ImportError:
     Timeout = None
 from kombu.async import timer as _timer
 from .base import apply_target, BasePool
 def apply_timeout(target, args=(), **rest):
-try:
+except ImportError:
     from gevent import Timeout
 except ImportError:
     Timeout = None
